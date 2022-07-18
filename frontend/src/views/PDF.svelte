9be507b7396df639d395e15da3314142d2b09622
minskapp <ignacia.rivas@alumnos.ucn.cl>
2022-07-17 23:16:48 -0400

    fix: Se comenta el código que no funciona.

--- frontend/src/views/PDF.svelte
+++ frontend/src/views/PDF.svelte
@@ -17,9 +17,9 @@
     onMount(searchAnimal);
     //const { jsPDF } = require("jspdf"); // will automatically load the node version
 
-    const doc = new jsPDF();
-    doc.text("Hello world!", 10, 10);
-    doc.save("a4.pdf"); // will save the file in the current working directory
+    //const doc = new jsPDF();
+    //doc.text("Hello world!", 10, 10);
+    //doc.save("a4.pdf"); // will save the file in the current working directory
     
     //var tex = "Prueba";
     //var doc = new jsPDF();        
