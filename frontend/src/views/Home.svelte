840253bd80a0438e9ee13c471ed247a1ae228c61
YasminUwu <yasmin.talamilla@alumnos.ucn.cl>
2022-07-17 20:47:03 -0400

    feat: Incorporación ruta PDF.svelte

--- frontend/src/views/Home.svelte
+++ frontend/src/views/Home.svelte
@@ -20,6 +20,9 @@
     function directRegister(){
         navigate('/register');
     }
+    function directPdf(){
+        navigate('/pdf');
+    }
 
 
 
@@ -29,16 +32,16 @@
 		let validation = $user.token
 
 		if(validation == 'e17326d066fdc4820582422babe431c8cf1e65f2'){
-			console.log("BIENVENIDO VETERINARIO");
+			
             isVeterinary = true;
 		}
         else if(validation == '8dcd6890b322daf80b2dc55e9fab83ddf2d6511f'){
-            console.log("BIENVENIDA SECRETARIA");
+            
             isSecretary = true;
         }
 		}
 	catch{
-		console.log("ERROR");
+		
 	}
 
     const items = [
@@ -74,14 +77,15 @@
         <CarouselControl direction="next" bind:activeIndex {items} />
     </Carousel>
     {#if isVeterinary}
-        <h1>BIENVENIDA VETERINARIO</h1>
+        <h2>BIENVENIDA VETERINARIO</h2>
         <button on:click={directView}>Lista de mascotas</button>
         <button on:click={directPet}>Buscar Mascotas</button>
         <button on:click={directRegister}>Registrar Mascota</button>
+        <button on:click={directPdf}>Emitir PDF</button>
     {/if}
 
     {#if isSecretary}
-        <h1>BIENVENIDA SECRETARIA</h1>
+        <h2>BIENVENIDA SECRETARIA</h2>
         <button on:click={directRegister}>Registrar Mascota</button>
     {/if}
 </div>
@@ -94,6 +98,15 @@
         color: #0050A0;
         font-family:'Lucida Sans';
         font-size: 3em;
+        text-transform: uppercase;
+        text-align: center;
+        text-decoration: underline;
+        text-shadow: 1px 1px 1px;
+    }
+    h2 {
+        color: #0050A0;
+        font-family:'Lucida Sans';
+        font-size: 2em;
         text-transform: uppercase;
         text-align: center;
         text-decoration: underline;
