ef7ed6a7972484335b7bd5f03dffdc17158a0a88
YasminUwu <yasmin.talamilla@alumnos.ucn.cl>
2022-07-17 22:42:24 -0400

    fix: Implementación de lógica para requerimiento reporte PDF

--- frontend/src/views/PDF.svelte
+++ frontend/src/views/PDF.svelte
@@ -1,26 +1,51 @@
+<script>
+    //import { jsPDF } from "jspdf";
+    import axios from 'axios';
+    import { onMount } from "svelte";
+    import {Table} from "sveltestrap";
+    var animalList = [];
 
+    /*
+    Esta función permite obtener el registro de una mascota
+    */
+    const searchAnimal = ()=>{
+        axios.get('http://127.0.0.1:8000/Listar-Mascotas/')
+        .then(res => {
+            animalList = res.data;
+        })
+    }
+    onMount(searchAnimal);
+    //const { jsPDF } = require("jspdf"); // will automatically load the node version
 
+    const doc = new jsPDF();
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf"); // will save the file in the current working directory
+    
+    //var tex = "Prueba";
+    //var doc = new jsPDF();        
+    //doc.text("holi" + tex, 10, 10);
+    //doc.save("a4.pdf");
+</script>
+
+<!--
 <head>
     <title>Registro de mascotas</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
-    <script>import axios from 'axios';</script>
+    <script src="https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js"></script>
 </head>
-
 <body>
-    <h1>holi</h1>
-    <script>
-        var listar;
-        const searchAnimal(){
-            axios.get('http://127.0.0.1:8000/Listar-Mascotas/')
-            .then(res => {
-                listar = res.data;
-            })
-        }
-        searchAnimal();
-        console.log(listar);
-        var tex = "JASINTA";
-        var doc = new jsPDF();        
-        doc.text("holi" + tex, 10, 10);
-        doc.save("a4.pdf");
-    </script>
+    {#await animalList}
+                Loading...
+    {:then animalList}
+        {#each animalList as animal }
+            {#if animal.status == "0"}
+                <script>
+                    doc.text(animal.ID);
+                    doc.save("a4.pdf");
+                </script>
+            {/if}
+        {/each}
+    {:catch error}
+    {error}
+    {/await} 
 </body>
+-->
